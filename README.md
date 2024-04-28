d7efa220734d77250f4a589d7752fffe7c97cd6d
cppHusky <cpphusky@outlook.com>
2024-04-28 18:34:49 +0800

    add-book-cover.sh can process any kind of image

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 - [rename.sh](rename.sh) - 重命名文件并移动到相应目录
 - [cover.sh](cover.sh) - 从 pdf 中提取封面（jpg 和 webp）
-- [add-pdf-cover.sh](add-pdf-cover.sh) - 向 pdf 中添加封面（png图片）
+- [add-pdf-cover.sh](add-pdf-cover.sh) - 向 pdf 中添加封面（各种图片格式）
--- add-book-cover.sh
+++ add-book-cover.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <pdf-file> <png-cover>"
+    echo "Usage: $0 <pdf-file> <cover-img>"
     exit 1
 fi
 ori=$1
-png=$2
+=$2
 dir=$(dirname "${ori}")
-convert $png 'png.pdf'
-pdftk 'png.pdf' $ori cat output 'tmp.pdf'
+convert $img 'img.pdf'
+pdftk 'img.pdf' $ori cat output 'tmp.pdf'
 md5=$(md5sum 'tmp.pdf' | cut -d ' ' -f 1)
 echo $md5
 destination="${dir}/${md5}.pdf"
 mv 'tmp.pdf' $destination
 echo "${ori} -> $destination"
-rm 'png.pdf'
+rm 'img.pdf'
